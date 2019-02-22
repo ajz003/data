217ca7a4d8c168732ae97a90081284254e96aac5
Anthony Zheng <ajz003@ucla.edu>
2019-02-21 21:39:49 -0800

    Initial Commit

---
+++ soccer.js
@@ -0,0 +1,65 @@
+var fs = require('fs')
+var readline = require('readline')
+
+const rl = readline.createInterface({
+    input: fs.createReadStream('soccer.dat')
+});
+
+var dataArr = []
+
+rl.on('line', (line) => {
+    // Potentially extracts team name, 'for' score, and 'against' score from each line
+
+    // Split lines on space delimiter and remove empty strings
+    let lineData = line.split(' ').filter(function (x) {
+        return x != ''
+    });
+
+    let scoreFor;
+    let scoreAgainst;
+    let teamName;
+
+    for (let i = 0, j = 0; i < lineData.length; i++) {
+        // To clean the numbers:
+        let potential = parseInt(lineData[i].replace(/[^0-9]/, ''))
+
+        if (i === 1) {
+            teamName = lineData[i]
+        }
+        if (i === 6) {
+            scoreFor = lineData[i]
+        }
+        if (i === 8) {
+            scoreAgainst = lineData[i]
+        }
+    }
+
+    let scoreObj = {
+        teamName: teamName,
+        scoreFor: scoreFor,
+        scoreAgainst: scoreAgainst,
+        spread: Math.abs(scoreFor - scoreAgainst)
+    }
+    if (scoreObj.teamName !== undefined && scoreObj.scoreFor !== undefined && scoreObj.scoreAgainst !== undefined) {
+        dataArr.push(scoreObj)
+    }
+
+})
+
+rl.on('close', () => {
+    // Finds the team with the minimum temperature spread, after the readstream closes
+
+    let minSpread = dataArr[0].spread
+    let minSpreadTeam;
+
+    for (let i = 0; i < dataArr.length; i++) {
+        let teamsSpread = dataArr[i].spread
+        if (teamsSpread < minSpread) {
+            minSpread = teamsSpread
+            minSpreadTeam = dataArr[i].teamName
+        }
+    }
+
+    console.log(minSpreadTeam + " has the smallest difference in 'for' and 'against' goals (" + (minSpread) + ").")
+
+})
---
+++ w_data.js
@@ -0,0 +1,73 @@
+var fs = require('fs')
+var readline = require('readline')
+
+const rl = readline.createInterface({
+    input: fs.createReadStream('w_data.dat')
+});
+
+var dataArr = []
+
+rl.on('line', (line) => {
+    // Potentially extracts day, minimum temperature, and maximum temperature from each line
+
+    // Split lines on space delimiter and remove empty strings
+    let lineData = line.split(' ').filter(function (x) {
+        return x != ''
+    });
+
+
+    let day;
+    let maxTemp;
+    let minTemp;
+
+    for (let i = 0; i < lineData.length; i++) {
+
+        // To clean the numbers:
+        let potential = parseInt(lineData[i].replace(/[^0-9]/, ''))
+
+        if (i === 0) {
+            day = lineData[i]
+        }
+        if (i === 1) {
+            maxTemp = lineData[i]
+        }
+        if (i === 2) {
+            minTemp = lineData[i]
+        }
+    }
+
+    let dayObj = {
+        day: day,
+        maxTemp: maxTemp,
+        minTemp: minTemp,
+        spread: maxTemp - minTemp
+    }
+
+    if (!isNaN(dayObj.day) && dayObj.minTemp !== undefined && dayObj.maxTemp !== undefined) {
+        dataArr.push(dayObj)
+    }
+
+})
+
+rl.on('close', () => {
+    // Finds the day with the minimum temperature spread, after the readstream closes
+
+    let minSpread = dataArr[0].spread
+    let minSpreadDay;
+
+    for (let i = 0; i < dataArr.length; i++) {
+
+        let daysSpread = dataArr[i].spread
+        if (daysSpread < minSpread) {
+            minSpread = daysSpread
+            minSpreadDay = dataArr[i].day
+        }
+
+        if (dataArr[i].day === 30) {
+            break;
+        }
+    }
+
+    console.log("Day " + minSpreadDay + " of the month has the smallest temperature spread (with a spread of " + minSpread + ").")
+
+})
